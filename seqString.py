9321b90f25d7e0cf198fb0f639d2dcb060f4f541
LiuXPeng <whyLiuXPeng@users.noreply.github.com>
2018-03-15 21:59:56 +0800

    sequence string
    finished

--- seqString.py
+++ seqString.py
@@ -8,9 +8,9 @@ __author__ = 'lxp'
 #实现《大话数据结构》128页“串”结构，书中每个函数绝大部分有函数说明
 
 class SeqString(object):
-	def __init__(self, maxSize, data = ""):
+	def __init__(self, maxSize = 100, data = ""):##
 		self.data = [None] * (maxSize + 1)
-		self.data[0] = maxSize
+		self.data[0] = max(maxSize, len(data))
 		self.length = 0
 		for x in data:
 			self.length = self.length + 1
@@ -21,12 +21,15 @@ class SeqString(object):
 
 	def add(self, data, index = None):
 		#给第index前面插入
-		if self.length == self.maxSize:
-			return
-		self.length = self.length + 1
+		if self.length == self.data[0]:
+			return		
 		if index == None:
+			self.length = self.length + 1
 			self.data[self.length] = data
 			return
+		if index > self.length + 1:
+			return
+		self.length = self.length + 1
 		for i in range(self.length - index):
 		#self.length已经++
 			self.data[self.length - i] = self.data[self.length - i - 1]
@@ -37,7 +40,7 @@ class SeqString(object):
 		if index < 1 or index > self.length:
 			return
 		for x in range(self.length - index):
-			self.data[self.length - index + x] = self.data[self.length - index + x + 1]
+			self.data[index + x] = self.data[index + x + 1]
 		self.length = self.length - 1
 		return
 
@@ -51,19 +54,25 @@ class SeqString(object):
 	def strLen(self):
 		return self.length
 
+	def showStr(self):
+		for x in range(self.length):
+			print(self.data[x + 1],' ',end = '')
+
 def isSeqStr(S):
 	return isinstance(S, SeqString)
 
+#把T复制给S
 def strCopy(T, S):
 	if not isSeqStr(S) or not isSeqStr(T):
 		return
-	S.length = min(S.maxSize, T.length)
-	for x in S.length:
+	S.length = min(S.data[0], T.length)
+	for x in range(S.length):
 		S.data[x + 1] = T.data[x + 1]
 	return
 
+#生成一个其值等于字符串的串
 def strAssign(data):
-	return T = SeqString(data, len(data))
+	return SeqString(len(data), data)
 
 def strCompare(T, S):
 	if not isSeqStr(S) or not isSeqStr(T):
@@ -80,12 +89,13 @@ def strCompare(T, S):
 	else:
 		return -1
 
+#连接S1， S2
 def concat(S1, S2):
 	T = SeqString(S1.length + S2.length)
 	for x in range(S1.length):
-		T.data[x + 1] = S1.data[x + 1]
+		T.add(S1.data[x + 1])
 	for x in range(S2.length):
-		T.data[S1.length + x + 1] = S1.data[x + 1]
+		T.add(S2.data[x + 1])
 	return T
 
 def subStr(S, pos, length):
@@ -93,38 +103,38 @@ def subStr(S, pos, length):
 		return
 	sub = SeqString(length)
 	for i in range(length):
-		length.data[i + 1] = S.data[pos + i]
+		sub.add(S.data[pos + i])
 	return sub
 
+#查找主串S中字串T在pos之后出现的位置
 def index(S, T, pos = 1):
-	if not isSeqStr(S) or not isSeqStr(T) or pos > S.length - T.length:
+	if not isSeqStr(S) or not isSeqStr(T) or pos > S.length - T.length + 1:
 		return
 	if S.length < T.length:
 		return
-	for x in range(S.length - T.length):
-		if x < pos - 1:
-			break
+	for x in range(S.length - T.length + 1):
+		if x + 1 < pos:
+			continue
 		for i in range(T.length):
-			if (S.data[x + i + 1] not = T.data[i + 1]):
+			if (S.data[x + i + 1] != T.data[i + 1]):
 				break
 			if i == T.length - 1:
-				return x
+				return x + 1
 	return
 
 def replace(S, T, V):
 	pos = index(S, T)
-	while pos not = None:
-		if S.length - T.length + V.length > S.maxSize:
+	while pos != None:
+		if S.length - T.length + V.length > S.data[0]:
 			return
-		for x in range(T.length):
-			S.delete(pos)
-		strInsert(S, pos, T)
-		pos = pos + T.length
+		strDelete(S, pos, T.length)
+		strInsert(S, pos, V)
+		pos = pos + V.length
 		pos = index(S, T, pos)
 	return
 
 def strInsert(S, pos, T):
-	if not isSeqStr(S) or not isSeqStr(T) or S.length + T.length > S.maxSize or pos < 1 or pos > S.length:
+	if not isSeqStr(S) or not isSeqStr(T) or S.length + T.length > S.data[0] or pos < 1 or pos > S.length:
 		return
 	for x in range(T.length):
 		S.add(T.data[x + 1], pos + x)
@@ -141,3 +151,87 @@ def strDelete(S, pos, length):
 	
 
 #test
+L = SeqString(data = 'abcdefgabcdefgabcdefgabcdefg')
+L.showStr()
+print('')
+
+L.add('e',5)
+L.showStr()
+print('')
+
+L.delete(3)
+L.showStr()
+print('')
+
+L.delete(3)
+L.showStr()
+print('')
+
+print(L.isStrEmpty())
+print('')
+print(L.strLen())
+'''
+L.cleanStr()
+L.showStr()
+print('')
+
+print(L.isStrEmpty())
+print('')
+'''
+print(isSeqStr(1))
+print(isSeqStr(L))
+
+print('copy')
+S = SeqString(7)
+strCopy(L, S)
+S.showStr()
+print('')
+
+A = strAssign('abc')
+A.showStr()
+print('')
+
+print(strCompare(A, L))
+
+A.showStr()
+L.showStr()
+print('')
+B = concat(A, L)
+B.showStr()
+print('')
+
+C = subStr(B, 1, 2)
+C.showStr()
+print('')
+
+
+print(index(B, C, 2))
+
+strInsert(L, 4, C)
+print('')
+L.showStr()
+
+'''
+print('')
+strDelete(L, 4, 2)
+print(index(L, C, 2))
+L.showStr()
+'''
+
+L.showStr()
+print('L')
+S.showStr()
+print('S')
+A.showStr()
+print('A')
+B.showStr()
+print('B')
+C.showStr()
+print('C')
+print('=============')
+D = SeqString(data='XXXXXX')
+L.showStr()
+print('L')
+replace(L, A, D)
+L.showStr()
+print('L')
