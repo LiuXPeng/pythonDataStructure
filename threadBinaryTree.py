34bfa4e5eaaadbf68cf71ce72d97c21a5669b4b1
LiuXPeng <whyLiuXPeng@users.noreply.github.com>
2018-04-18 16:29:50 +0800

    ebook

--- threadBinaryTree.py
+++ threadBinaryTree.py
@@ -8,4 +8,16 @@ __author__ = 'lxp'
 #《大话数据结构》191页
 
 class BiThrNode(object):
-	
+	def __init__(self, data):
+		self.data = data
+		self.lchild = None
+		self.rchild = None
+		self.ltag = None
+		self.rtag = None
+
+class ThBiTree(object):
+	def __init__(self):
+		self.head = None
+
+#中序遍历进行中序线索化
+	def inThreading(BT):
