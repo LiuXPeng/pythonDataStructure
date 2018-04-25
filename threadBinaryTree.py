5ed05cb55d034b7ad0dc4a904a713b8e8dbd74c6
LiuXPeng <whyLiuXPeng@users.noreply.github.com>
2018-04-25 11:53:13 +0800

    thread binary tree

--- threadBinaryTree.py
+++ threadBinaryTree.py
@@ -19,7 +19,8 @@ class BiThrNode(object):
 
 class ThBiTree(object):
 	def __init__(self):
-		self.head = None
+		self.lchild = None
+		self.rchild = None
 
 #中序遍历进行中序线索化
 def inThreading(p):
@@ -36,3 +37,43 @@ def inThreading(p):
 				pre.rchild = p
 			pre = p
 			method(p.rchild)
+		return
+
+def InOrderTraverse_Thr(T):
+	p = T.lchild
+	while p ! = T:
+		while p.ltag == False:
+			p = p.lchild
+		print(P.data, ' ', end = '')
+		while p.rtag == True:
+			p = p.rchild
+			print(p.data, ' ', end = '')
+		p = p.rchild
+	return
+
+#二叉树转化为线索二叉树
+def Bitree2Thbitree(BT):
+	res = ThBiTree()
+	if BT.head == None:
+		res.head = None
+		return res
+	res.lchild = BiThrNode(BT.head.data)
+	def method(BNode, TNode):
+		if BNode.rchild != None:
+			TNode.rchild = BiThrNode(BNode.rchild.data)
+			method(BNode.rchild, TNode.rchild)
+		if BNode.lchild != None:
+			TNode.lchild = BiThrNode(BNode.lchild.data)
+			method(BNode.lchild, TNode.lchild)
+		return
+	method(BT.head, res.head)
+	inThreading(res)
+	mark = res.lchild
+	while mark.lchild != None:
+		mark = mark.lchild
+	mark.lchild = lchild
+	while mark.rchild != None:
+		mark = mark.rchild
+	res.rchild = mark
+	mark.rchild = res
+	return res
