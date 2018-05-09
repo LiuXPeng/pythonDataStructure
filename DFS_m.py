432b079f6abe91a5a74c01ec7770ec77642d584d
LiuXPeng <LiuXPeng@users.noreply.github.com>
2018-05-09 10:45:19 +0800

    dfs

---
+++ DFS_m.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'depth first search'
+
+__author__ = 'lxp'
+
+#《大话数据结构》240页
+
+
+
+
+
+#test
+def test():
+	pass
+	return
+
+if __name__ == '__main__':
+	test()
+return
