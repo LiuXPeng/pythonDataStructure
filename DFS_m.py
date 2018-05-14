8f17632e075995d72383e6064fa0c097dbe28ad3
LiuXPeng <LiuXPeng@users.noreply.github.com>
2018-05-14 17:50:12 +0800

    depth first search(m)

--- DFS_m.py
+++ DFS_m.py
@@ -7,15 +7,32 @@ __author__ = 'lxp'
 
 #《大话数据结构》240页
 
+import adjacencyMatrix
 
+def DFS(G, i):
+	global visited
+	visited[i] = True
+	print(G.vexs[i])
+	for j in range(G.numVertexes):
+		if G.arc[i][j] == 1 and not visited[j]:
+			DFS(G, j)
 
+def DFSTraverse(G):
+	global visited
+	visited = [False] * G.numVertexes
+	for i in range(G.numVertexes):
+		if not visited[i]:
+			DFS(G, i)
+		
 
 
 #test
 def test():
-	pass
+	sample = adjacencyMatrix.MGraph()
+	sample.createMGraph()
+	sample.showMGraph()
+	DFSTraverse(sample)
 	return
 
 if __name__ == '__main__':
 	test()
-return
