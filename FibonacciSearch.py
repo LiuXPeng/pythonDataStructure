980ed4e2f1b6cd51864f714127a123441e4a610b
LiuXPeng <LiuXPeng@users.noreply.github.com>
2018-05-22 22:10:51 +0800

    Fibonacci search

---
+++ FibonacciSearch.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'FibonacciSearch'
+
+__author__ = 'lxp'
+
+#《大话数据结构》303页
+
+def FibonacciSearch():
+	pass
+	return
+
+
+#test
+def test():
+	key = int(input("请输入key"))
+	L = [0,1,2,3,4,5,6,7]
+	print(FibonacciSearch(L, key))
+	return
+
+if __name__ == '__main__':
+	test()
