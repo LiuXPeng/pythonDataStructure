3885d6becb67f19be8377a4d3d5a65cbfbcddda1
LiuXPeng <LiuXPeng@users.noreply.github.com>
2018-05-23 15:56:54 +0800

    Fibonacci search

--- FibonacciSearch.py
+++ FibonacciSearch.py
@@ -1,21 +1,51 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'FibonacciSearch'
+'Fibonacci search'
 
 __author__ = 'lxp'
 
 #《大话数据结构》303页
 
-def FibonacciSearch():
-	pass
+def Fbi(i):
+	if i < 2:
+		return i
+	return Fbi(i - 1) + Fbi(i - 2)
+
+def FibonacciSearch(a, key):
+	n = len(a) - 1
+	low = 0
+	high = n
+	k = 0
+	while n > Fbi(k):
+		k = k + 1
+	for i in range(n, Fbi(k) - 1):
+		a.append(a[-1])
+
+	while low < high:
+		mid = low + Fbi(k - 1) + 1
+		if key < a[mid]:
+			high = mid - 1
+			k = k - 1
+		elif key > a[mid]:
+			low = mid + 1
+			k = k - 2
+		else:
+			if mid < n:
+				return mid
+			else:
+				return n
+
+	if key == a[low]:
+		return low
+
 	return
 
 
 #test
 def test():
 	key = int(input("请输入key"))
-	L = [0,1,2,3,4,5,6,7]
+	L = [0, 1, 2, 3, 4]
 	print(FibonacciSearch(L, key))
 	return
 
