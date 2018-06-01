7a1e481410b9b0ca9b8bfe8262d499e4bc579436
LiuXPeng <LiuXPeng@users.noreply.github.com>
2018-06-01 18:37:26 +0800

    Self-balancing binary search tree

--- AVL.py
+++ AVL.py
@@ -14,14 +14,14 @@ class BiTNode(object):
 		self.lchild = None
 		self.rchild = None
 
-def r_Rotate(P):
+def r_Rotate(p):
 	L = BiTNode()
 	L = p.lchild
 	p.lchild = L.rchild
 	L.rchild = p
 	return L
 
-def l_Rotate(P):
+def l_Rotate(p):
 	R = BiTNode()
 	R = p.rchild
 	p.rchild = R.lchild
@@ -37,9 +37,8 @@ def leftBalance(T):
 		Lr = L.rchild
 		if Lr.bf == 1:
 			L.bf = 0
-			break
 		elif L.bf == 0:
-			break
+			pass
 		else:
 			L.bf = 1
 		Lr.bf = 0
@@ -50,33 +49,91 @@ def leftBalance(T):
 def rightBalance(T):
 	R = T.rchild
 	if R.bf == -1:
-		T.bf = r.bf = 0
+		T.bf = R.bf = 0
 		T = l_Rotate(T)
 	if R.bf == 1:
 		Rl = R.lchild
 		if Rl.bf == -1:
 			R.bf = 0
 		elif Rl.bf == 0:
-			break
+			pass
 		else:
 			R.bf == -1
-			break
 		Rl.bf = 0
 		T.rchild = r_Rotate(T.rchild)
 		T = l_Rotate(T)
 	return T
 
-def insertAVL(T, e):
-	if T:
-		pass
-	else:
+def insertAVL(T, e, taller, mark):
+	if not T:
 		T = BiTNode()
 		T.data = e
-	return T
+		T.bf = 0
+		taller = True
 
+
+	else:
+		if e == T.data:
+			taller = False
+			return T, e, taller, False
+
+		if e < T.data:
+			T.lchild, e, taller, mark = insertAVL(T.lchild, e, taller, mark)
+			if not mark:
+				return T, e, taller, False
+			if taller:
+				if T.bf == 1:
+					T = leftBalance(T)
+					taller = False
+				elif T.bf == 0:
+					T.bf = 1
+					taller = True
+				else:
+					T.bf = 0
+					taller = False
+
+		else:
+			T.rchild, e, taller, mark = insertAVL(T.rchild, e, taller, mark)
+			if not mark:
+				return T, e, taller, False
+			if taller:
+				if T.bf == 1:
+					T.bf = 0
+					taller = False
+				elif T.bf == 0:
+					T.bf = -1
+					taller = True
+				else:
+					T = rightBalance(T)
+					taller = False
+
+	return T, e, taller, True
+
+def showTree(T):
+	if not T:
+		print("树为空树")
+		return
+	L = [T]
+	while len(L) != 0:
+		print(L[0].data, ',', end = '')
+		if L[0].lchild:
+			L.append(L[0].lchild)
+		if L[0].rchild:
+			L.append(L[0].rchild)
+		del L[0]
+	print()
+	return
+
+
+			
 #test
 def test():
-	pass
+	L = [3, 2, 1, 4, 5, 6, 7, 10, 9, 8]
+	T = None
+	for i in L:
+		T, e, taller, mark = insertAVL(T, i, False, False)
+		showTree(T)
+	#showTree(T)
 	return
 
 if __name__ == '__main__':
